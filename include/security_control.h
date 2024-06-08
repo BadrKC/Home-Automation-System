70a5c4671909ca413f22b3bc00bd9cdf4024af3f
KacemiBadr <badr.kacemi@uit.ac.ma>
2024-06-08 21:52:28 +0100

    Implemented Core Security System Control Functions

---
+++ include/security_control.h
@@ -0,0 +1,12 @@
+#ifndef SECURITY_SYSTEM_H
+#define SECURITY_SYSTEM_H
+
+#define ON 1
+#define OFF 0
+
+// Function Declarations
+void armSystem();
+void disarmSystem();
+void checkSystemStatus();
+
+#endif // SECURITY_SYSTEM_H
---
+++ src/security_control.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "../include/security_control.h" // Include header 
+
+    // * Implement basic security system control functions (e.g., `armSystem`, `disarmSystem`, `checkSystemStatus`).
+    // * Implement function to display security system status.
+    // * Test security system functionalities.
+
+bool securityState = OFF;
+
+void armSystem(){
+    securityState = ON;
+    // printf("Security system activated.\n");
+}
+void disarmSystem(){
+    securityState = OFF;
+    // printf("Security system deactivated.\n");
+}
+
+void checkSystemStatus(){
+    if (securityState == ON)
+    {
+        printf("Security system activate.\n");
+
+    }else
+    {
+        printf("Security system inactivate.\n");
+    }
+    
+}
+// for testing purpose
+
+int main(){
+    armSystem();
+    checkSystemStatus();
+    disarmSystem();
+    checkSystemStatus();
+}
